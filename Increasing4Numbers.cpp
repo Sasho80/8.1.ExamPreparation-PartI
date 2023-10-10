5400d0dc05d7a0eedd56e98cc9737ffdd92972ff
Aleksandar <Sasho80@gbg.bg>
2023-10-10 21:55:37 +0300

    Add files via upload

---
+++ Increasing4Numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int conter = 0;
+	for (int i = a; i <=b; i++){
+		for  (int j = i+1; j <=b; j++){
+			for ( int k = j+1; k <=b; k++){
+				for (int l = k+1; l <=b; l++){
+					cout << i << " " << j << " " << k << " " << l << endl;
+					conter++;
+				}
+
+			}
+		}
+	}
+	if (conter==0){
+		cout << "No" << endl;
+	}
+	return 0;
+}
