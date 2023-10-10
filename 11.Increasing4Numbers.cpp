0822449d6c8a38f2d26ef8eeb4b89ec16894a938
Aleksandar <Sasho80@gbg.bg>
2023-10-10 22:18:48 +0300

    Update 11.Increasing4Numbers.cpp

--- 11.Increasing4Numbers.cpp
+++ 11.Increasing4Numbers.cpp
@@ -3,18 +3,18 @@ using namespace std;
 int main() {
 	int a, b;
 	cin >> a >> b;
-	int conter = 0;
+	int counter = 0;
 	for (int i = a; i <=b; i++){
 		for  (int j = i+1; j <=b; j++){
 			for ( int k = j+1; k <=b; k++){
 				for (int l = k+1; l <=b; l++){
 					cout << i << " " << j << " " << k << " " << l << endl;
-					conter++;
+					counter++;
 				}
 			}
 		}
 	}
-	if (conter==0){
+	if (counter==0){
 		cout << "No" << endl;
 	}
 	return 0;
